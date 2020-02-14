161dd2616b3f6b52c315eddedaa4aa84ad36cbb1
Rohini Senthil <rohinivsenthil@gmail.com>
2020-02-14 11:30:38 +0530

    Resolved warnings

--- src/components/Container/index.test.js
+++ src/components/Container/index.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { act } from '@testing-library/react-hooks'
-import { render, fireEvent, wait } from '@testing-library/react';
+import {act} from '@testing-library/react-hooks'
+import { render, fireEvent, wait, waitForDomChange } from '@testing-library/react';
 import Container from '../Container';
 import axios from 'axios';
 import url from '../../constants/url'
@@ -11,14 +11,19 @@ jest.mock('axios', () => ({
 }));
 
 describe('The container component',()=>{
-    it('should be rendered correctly',()=>{
+    it('should be rendered correctly',async ()=>{
         const {asFragment}=render(<Container testId='test-cntner'/>)
+        await waitForDomChange();
         expect(asFragment()).toMatchSnapshot();
     });
 
-    it('should pass the input text entered to the button',()=>{
+    it('should pass the input text entered to the button', async()=>{
         const {getByTestId}=render(<Container testId='test-cntner' testIdButton='test-btn' testIdTextBox='123'/>)
-        fireEvent.change(getByTestId('123'),{target:{value:'Bhumika'}});
+        await waitForDomChange();
+        act(() => {
+            fireEvent.change(getByTestId('123'),{target:{value:'Bhumika'}})
+            return undefined;
+        });
         expect(getByTestId('test-btn')).toHaveTextContent('Bhumika clicked 0 times.');
     })
 
